2384c21dd1bfd614a608aae770bdf08ea1e3aee1
ForgottenRat <75881555+ForgottenRat@users.noreply.github.com>
2022-03-25 22:15:03 +0200

    Add files via upload

--- splitter.py
+++ splitter.py
@@ -1,15 +1,13 @@
 order = input("Alphabet order/number order? yes/no: ")
 ans = input("eneter word/letter: ")
 print('seperating ' +(ans))
+word = list(ans)
 
 #seperates word into a list or alphabet/number order
+
 if order == "no":
-    word = list(ans)
     print(word)
     
 if order == "yes":
-    word = list(ans)
     list.sort(word)
     print(word)
-
-
